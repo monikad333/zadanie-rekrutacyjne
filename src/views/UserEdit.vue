03aedb56d45a51e50e51e58a6d81ecff81853dfa
Monika Duda <monduda@student.agh.edu.pl>
2023-07-10 17:59:02 +0200

    v-for refactor

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -2,6 +2,29 @@
 import ProjectButton from '@/components/ProjectButton.vue'
 import DefaultCard from '@/components/DefaultCard.vue'
 import SmallButton from '@/components/SmallButton.vue'
+import { onMounted, ref } from 'vue'
+
+const permissions = ref()
+
+onMounted(() => {
+  permissions.value = [
+    {
+      name: 'Script'
+    },
+    {
+      name: 'Props'
+    },
+    {
+      name: 'Screens'
+    },
+    {
+      name: 'Money'
+    },
+    {
+      name: 'Stunt'
+    }
+  ]
+})
 </script>
 
 <template>
@@ -23,100 +46,47 @@ import SmallButton from '@/components/SmallButton.vue'
         <template #top>
           <h1 class="text-2xl px-10 py-10">Role: <span class="text-lg">Performer</span></h1>
         </template>
-        <template #bottom><div class="table-section bg-white border-t border-gray-300">
-          <div class="relative overflow-x-auto px-2">
-            <table class="w-full text-sm text-left text-gray-500 text-gray-400">
-              <thead class="text-xs border-b text-gray-700 bg-gray-50 bg-white-700 border-gray-700">
-                <tr>
-                  <th scope="col" class="px-6 py-3 w-1/2">Object</th>
-                  <th scope="col" class="px-6 py-3 w-1/2">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="bg-white border-b bg-white-800">
-                  <th
-                    scope="row"
-                    class="px-6 uppercase py-4 font-semibold text-gray-700 whitespace-nowrap text-gray"
-                  >
-                    movies
-                  </th>
-                  <!-- <td class="px-6 py-4">Silver</td> -->
-                </tr>
-                <tr class="bg-white border-b bg-white-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap text-gray"
-                  >
-                    Script
-                  </th>
-                  <td class="px-6 py-4 row space-x-1">
-                    <SmallButton btn-color="#d7d7d7" btn-content="create"></SmallButton>
-                    <SmallButton btn-color="#75a9c8" btn-content="read"></SmallButton>
-                    <SmallButton btn-color="#1b8e88" btn-content="update"></SmallButton>
-                    <SmallButton btn-color="#ce1d1d" btn-content="delete"></SmallButton>
-                  </td>
-                </tr>
-                <tr class="bg-white border-b bg-white-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap text-gray"
-                  >
-                    Props
-                  </th>
-                  <td class="px-6 py-4 row space-x-1">
-                    <SmallButton btn-color="#d7d7d7" btn-content="create"></SmallButton>
-                    <SmallButton btn-color="#75a9c8" btn-content="read"></SmallButton>
-                    <SmallButton btn-color="#1b8e88" btn-content="update"></SmallButton>
-                    <SmallButton btn-color="#ce1d1d" btn-content="delete"></SmallButton>
-                  </td>
-                </tr>
-                <tr class="bg-white border-b bg-white-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap text-gray"
-                  >
-                    Screens
-                  </th>
-                  <td class="px-6 py-4 row space-x-1">
-                    <SmallButton btn-color="#d7d7d7" btn-content="create"></SmallButton>
-                    <SmallButton btn-color="#75a9c8" btn-content="read"></SmallButton>
-                    <SmallButton btn-color="#1b8e88" btn-content="update"></SmallButton>
-                    <SmallButton btn-color="#ce1d1d" btn-content="delete"></SmallButton>
-                  </td>
-                </tr>
-                <tr class="bg-white border-b bg-white-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap text-gray"
-                  >
-                    Money
-                  </th>
-                  <td class="px-6 py-4 row space-x-1">
-                    <SmallButton btn-color="#d7d7d7" btn-content="create"></SmallButton>
-                    <SmallButton btn-color="#75a9c8" btn-content="read"></SmallButton>
-                    <SmallButton btn-color="#1b8e88" btn-content="update"></SmallButton>
-                    <SmallButton btn-color="#ce1d1d" btn-content="delete"></SmallButton>
-                  </td>
-                </tr>
-                <tr class="bg-white border-b bg-white-800">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap text-gray"
-                  >
-                    Stunts
-                  </th>
-                  <td class="px-6 py-4 row space-x-1">
-                    <SmallButton btn-color="#d7d7d7" btn-content="create"></SmallButton>
-                    <SmallButton btn-color="#75a9c8" btn-content="read"></SmallButton>
-                    <SmallButton btn-color="#1b8e88" btn-content="update"></SmallButton>
-                    <SmallButton btn-color="#ce1d1d" btn-content="delete"></SmallButton>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div></template>
-        
+        <template #bottom
+          ><div class="table-section bg-white border-t border-gray-300">
+            <div class="relative overflow-x-auto px-2">
+              <table class="w-full text-sm text-left text-gray-500 text-gray-400">
+                <thead
+                  class="text-xs border-b text-gray-700 bg-gray-50 bg-white-700 border-gray-700"
+                >
+                  <tr>
+                    <th scope="col" class="px-6 py-3 w-1/2">Object</th>
+                    <th scope="col" class="px-6 py-3 w-1/2">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="bg-white border-b bg-white-800">
+                    <th
+                      scope="row"
+                      class="px-6 uppercase py-4 font-semibold text-gray-700 whitespace-nowrap text-gray"
+                    >
+                      movies
+                    </th>
+                    <!-- <td class="px-6 py-4">Silver</td> -->
+                  </tr>
+                  <tr v-for="permission of permissions" class="bg-white border-b bg-white-800">
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-xs text-gray-900 whitespace-nowrap text-gray"
+                    >
+                      {{ permission.name }}
+                    </th>
+                    <td class="px-6 py-4 row space-x-1">
+                      <SmallButton btn-color="#d7d7d7" btn-content="create"></SmallButton>
+                      <SmallButton btn-color="#75a9c8" btn-content="read"></SmallButton>
+                      <SmallButton btn-color="#1b8e88" btn-content="update"></SmallButton>
+                      <SmallButton btn-color="#ce1d1d" btn-content="delete"></SmallButton>
+                    </td>
+                  </tr>
+                  
+                </tbody>
+              </table>
+            </div></div
+        ></template>
       </DefaultCard>
       <DefaultCard left-content="Załączniki" right-content="Historia" class="card-right">
         vsadvcsdvsd
